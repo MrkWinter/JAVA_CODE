bb52687a94410239415a9ecb5eb83fc606d5a34d
MrkWinter <744988414@qq.com>
2023-02-21 23:04:15 +0800

    java学习(2)

---
+++ java学习(2)/changechar.java
@@ -0,0 +1,13 @@
+public class changechar {
+
+	public static void main(String[] args) {
+		System.out.println("北京\t天津\t上海");//北京    天津    上海
+		System.out.println("it is very \nbeautiful");//it is very 
+		//beautiful
+		System.out.println("this is a \\");//this is a \
+		System.out.println("\"老师说要好好学习\"");//"老师说要好好学习"
+		System.out.println("he  like me\rshe");//she like me
+		//练习
+		System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+	}
+}
---
+++ java学习(2)/comment.java
@@ -0,0 +1,19 @@
+
+//文档注释
+/**
+ * @author MrkWinter
+ * @version 1.0
+ */
+
+public class comment {
+	public static void main(String[] args) {
+
+			//单行注释
+			/*  
+
+	        多行注释
+
+			*/
+
+	}
+}
